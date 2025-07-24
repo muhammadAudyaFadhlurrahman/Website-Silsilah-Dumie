49d1560a38552a14b7d0b9d9bc11467ee7990112
muhammadAudyaFadhlurrahman <audyafadhlurrahman@gmail.com>
2025-07-24 19:05:33 +0700

    another update

---
+++ src/modules/components/Silsilah/DiagramViewer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DiagramViewer() {
+  return (
+    <div>
+      <img className="family-tree-img" src="/images/silsilahDumie.png" alt="Dumie Family Tree" />
+    </div>
+  );
+}
+
+export default DiagramViewer;
--- src/modules/components/Silsilah/Silsilah.js
+++ src/modules/components/Silsilah/Silsilah.js
@@ -1,9 +1,18 @@
 import React from "react";
+import DiagramViewer from "./DiagramViewer";
 
 function Silsilah() {
   return (
-    <div className="container-silsilah">
-      <h1>Silsilah Page</h1>
+    <div className="container-silsilah flex-column">
+      <h1>Silsilah Keluarga Dumie</h1>
+      <div>
+        <DiagramViewer />
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <a href="/images/silsilahDumie.png" download target="_blank" rel="noopener noreferrer">
+          <button className="btn-download">Download PNG</button>
+        </a>
+      </div>
     </div>
   );
 }
